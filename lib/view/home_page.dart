588e60fcc1229980786bbcabe59561ae2701b828
salstein <saloluwaseun@gmail.com>
2022-05-18 12:13:14 +0100

    Home page

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -2,35 +2,19 @@ import "package:flutter/material.dart";
 import 'package:get/get.dart';
 import 'package:symfonia/controllers/response_Controller.dart';
 import 'package:symfonia/styles.dart';
+import 'package:symfonia/view/loading_page.dart';
 import 'package:symfonia/view/widgets.dart';
 
 
-
-
-
-
+CoinController coinController = Get.put(CoinController());
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: dashBack, appBar: appBar(), body:coinController.obx((state){return appBody();
 
-
-
-    return Obx(  //Obx used to observe any change in the value in the coin controller.
-      () => Scaffold(
-          backgroundColor: dashBack,
-          appBar: appBar(),
-          body: CoinController().isLoading.value
-              ? SizedBox(
-                  child: Center(
-                  child: Column(
-                    children: const [
-                      CircularProgressIndicator(color: Colors.green),
-                    ],
-                  ),
-                ))
-              : appBody()),
-    );
+        }, onLoading:const Loading()) );
   }
 }
