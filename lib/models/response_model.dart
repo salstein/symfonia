5ef617b38404b1c686e39b80782988bbd19b294a
Salako Oluwaseun <saloluwaseun@gmail.com>
2022-05-18 11:19:35 +0100

    Update response_model.dart

--- lib/models/response_model.dart
+++ lib/models/response_model.dart
@@ -131,33 +131,3 @@ class Coin {
       };
 }
 
-
-final bitcoin = {
-  "id": "bitcoin",
-  "symbol": "btc",
-  "name": "Bitcoin",
-  "image":
-      "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
-  "current_price": 43569,
-  "market_cap": 827166549800,
-  "market_cap_rank": 1,
-  "fully_diluted_valuation": 915611962104,
-  "total_volume": 32314478253,
-  "high_24h": 44199,
-  "low_24h": 38064,
-  "price_change_24h": 5208.65,
-  "price_change_percentage_24h": 13.57818,
-  "market_cap_change_24h": 98944368242,
-  "market_cap_change_percentage_24h": 13.58711,
-  "circulating_supply": 18971462.0,
-  "total_supply": 21000000.0,
-  "max_supply": 21000000.0,
-  "ath": 69045,
-  "ath_change_percentage": -37.00762,
-  "ath_date": "2021-11-10T14:24:11.849Z",
-  "atl": 67.81,
-  "atl_change_percentage": 64040.37149,
-  "atl_date": "2013-07-06T00:00:00.000Z",
-  "roi": null,
-  "last_updated": "2022-03-01T11:18:57.826Z"
-};
